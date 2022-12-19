da482f45ebcdb83048905aa397615868980855ae
Gui37 <guifty32@gmail.com>
2022-12-19 02:57:46 +0200

    Endpoint UPDATE para categoria

--- src/main/java/mz/com/library/service/CategoriaService.java
+++ src/main/java/mz/com/library/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mz.com.library.domain.Categoria;
+import mz.com.library.dtos.CategoriaDTO;
 import mz.com.library.repositories.CategoriaRepository;
 import mz.com.library.service.exceptions.ObjectNotFoundException;
 
@@ -14,19 +15,27 @@ import mz.com.library.service.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria findById(Integer id) {
-		Optional <Categoria> obj = categoriaRepository.findById(id); //Evita o NullPointer error
+		Optional<Categoria> obj = categoriaRepository.findById(id); // Evita o NullPointer error
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Categoria/Objecto não encontrado! ID: " + id + " do Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setTitulo(objDTO.getTitulo());
+		obj.setDescricao(objDTO.getDescricao());
+
 		return categoriaRepository.save(obj);
 	}
 }
