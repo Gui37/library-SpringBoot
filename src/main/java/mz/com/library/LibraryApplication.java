c6c1a8e0edb93557d596d9af862fb7cbf73c86d8
Gui37 <guifty32@gmail.com>
2023-03-01 20:03:29 +0200

    Trying to fix CORS policy

--- src/main/java/mz/com/library/LibraryApplication.java
+++ src/main/java/mz/com/library/LibraryApplication.java
@@ -2,10 +2,7 @@ package mz.com.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 public class LibraryApplication {
@@ -13,15 +10,6 @@ public class LibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/categoria").allowedOrigins("*");
-				registry.addMapping("/livro").allowedOrigins("*");
-			}
-		};
-	}
+
 }
 
