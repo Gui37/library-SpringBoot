e62624c60fb9a65612f0caaceb99616d19eaacaf
Gui37 <guifty32@gmail.com>
2023-03-02 04:20:30 +0200

    Trying to fix CORS policy

--- src/main/java/mz/com/library/LibraryApplication.java
+++ src/main/java/mz/com/library/LibraryApplication.java
@@ -3,7 +3,6 @@ package mz.com.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class LibraryApplication {
 
@@ -12,4 +11,3 @@ public class LibraryApplication {
 	}
 
 }
-
