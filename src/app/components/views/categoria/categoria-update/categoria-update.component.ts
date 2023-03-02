822231887bac91e3b2bb4e899d7f772dce1d24e8
Gui37 <guifty32@gmail.com>
2023-03-02 23:21:34 +0200

    Metodo update para categoria implementado

--- src/app/components/views/categoria/categoria-update/categoria-update.component.ts
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriaService } from '../../categoria.service';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-update',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./categoria-update.component.css']
 })
 export class CategoriaUpdateComponent {
+  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router) { }
 
+  categoria: Categoria = {
+    id: "",
+    titulo: "",
+    descricao: ""
+  }
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  update():void {
+    this.service.update(this.categoria).subscribe((resposta) =>{
+      this.router.navigate(['categorias'])
+      this.service.mensagem("Categoria actualizada com sucesso")
+    }, err => {
+      this.service.mensagem("Verifique se todos os campos estão preenchidos")
+    })
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe(resposta => {
+      this.categoria.titulo = resposta.titulo
+      this.categoria.descricao = resposta.descricao
+      console.log(this.categoria)
+    })
+  }
+  cancel(): void {
+    this.router.navigate(['categorias'])
+  }
 }
